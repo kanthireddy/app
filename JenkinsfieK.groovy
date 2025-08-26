593a2ced8a475093d0524edf867d1a8c98a54e78
Kanthi <kanthikumarreddy2002@gmail.com>
2025-08-26 06:11:30 -0700

    success

---
+++ JenkinsfieK.groovy
@@ -0,0 +1,43 @@
+pipeline{
+
+    agent any
+
+    toold{
+        maven 'Maven'
+        jdk 'JDK'
+
+    }
+
+    environment{
+        ANSIBLE_HOST ="ubuntu@18.236.177.29"
+        SSH_KEY_ID ="anisble-key"
+    }
+
+    stages{
+        stage("clone rep"){
+        steps{
+
+            git url:"https://github.com/kanthireddy/app.git"
+        }
+    }
+    
+    stage("Build"){
+        steps{
+            sh "mvn clean package"
+        }
+    }
+
+    stage("Deploy"){
+        steps{
+            sshagent(credentails: ['ansible-key']){
+                sh '''
+ 
+                    ANSIBLE_HOST_KEY_CHECKING=False ansible playbook -i hosts.ini deploy.yml
+
+                '''
+            }
+        }
+    }
+
+
+}
